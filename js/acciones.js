5aa75d97a9acde44f93efb2a154153f4fd2353f9
AL35F13 <anahi2028@gmail.com>
2015-06-02 13:45:40 -0500

    tres
    se inserto audio a las acciones

--- js/acciones.js
+++ js/acciones.js
@@ -1,6 +1,16 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
+	
+	audio=window.plugins.LowLatencyAudio;
+	audio.preloadFX('B1', 'audio/C.mp3', function(){}, function(msg){alert("Error " + msg);}); 
+	
+	audio.preloadFX('B2', 'audio/D.mp3', function(){}, function(msg){alert("Error " + msg);});
+	
+	audio.preloadFX('B3', 'audio/E.mp3', function(){}, function(msg){alert("Error " + msg);});
+	
+	audio.preloadFX('B4', 'audio/F.mp3', function(){}, function(msg){alert("Error " + msg);});
+	
 	$('#btnjugar').on('tap',function(){
 		var pantalla=$.mobile.gatScreenHeight();
 		var encabezado=$('.ui-header').outerHeight();
@@ -25,6 +35,7 @@ document.addEventListener("deviceready",function(){
 	
 	function quien(q)
 	{
+		audio.play(q);
 		return q.substring(1);
 	}
 	
